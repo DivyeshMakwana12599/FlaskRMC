92bdad4be2455602370d8179513b0b937f28312e
DivyeshMakwana12599 <divyeshmakwana12599@gmail.com>
2021-10-12 22:02:03 +0530

    venv updated and methods added to esp8266_json

--- esp8266_json/esp8266_json.ino
+++ esp8266_json/esp8266_json.ino
@@ -9,10 +9,13 @@
 #pragma message(THIS EXAMPLE IS FOR ESP8266 ONLY!)
 #error Select ESP8266 board.
 #endif
-const String url = "http://e050-1-38-182-39.ngrok.io";
+
 DHTesp dht;
-const char* ssid = "Juhi";
-const char* pass = "juhi1010";
+
+const String url = "http://792f-8-17-206-80.ngrok.io";
+const char* ssid = "ASUS_ROG";
+const char* pass = "16013010";
+
 
 void SetupPipeController(){
   Serial.begin(115200);
@@ -37,6 +40,26 @@ void SetupPipeController(){
 }
 
 
+void getRequestPipe(String s){
+  HTTPClient http;
+  WiFiClient wifi;
+  String path = url +  s;
+  http.begin(wifi, path);
+  int statusCode = http.GET();
+    if (statusCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(statusCode);
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(statusCode);
+    }
+    http.end();
+}
+
+
 String createJsonString(){
     float temp = dht.getTemperature();
     DynamicJsonDocument root(200);
@@ -48,29 +71,24 @@ String createJsonString(){
 
 
 
+
 void setup() {
   SetupPipeController();
 }
 
 
 
-void loop() {
-  if(Serial.read() == 'g'){
-    HTTPClient http;
-    WiFiClient wifi;
-    String path = url +  "/Rajkot/Area1";
-    http.begin(wifi, path);
-    int statusCode = http.GET();
 
-      if (statusCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(statusCode);
-        String payload = http.getString();
-        Serial.println(payload);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(statusCode);
-      }
+void loop() {
+  if(Serial.read() == 'p'){
+    WiFiClient wifi;
+    HTTPClient http;
+    http.addHeader("Content-Type", "application/json");
+    String path = url + "/Umbergaon/NewGIDC/DivyeshHouse";
+    http.begin(wifi, path);
+    int statusCode = http.POST(createJsonString());
+    Serial.print("HTTP Response code: ");
+    Serial.println(statusCode);
+    http.end();
   }
 }
